e014d689fd2de9bbe1c77125c2f421d29a877cd2
icpac <cj.icpac@gmail.com>
2021-09-01 15:58:57 -0500

    Module

---
+++ Facturador.Module/BusinessObjects/Cliente.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facturador.Module.BusinessObjects
+{
+    public class Cliente
+    {
+        string Rfc { get; set; }
+        string Nombre { get; set; }
+    }
+}
